d4e2d92b5c163aa4488280842c8a2954e0f4127e
lywoo-plus <sreng.lywoo@gmail.com>
2026-08-21 17:29:06 +0700

    feat: add loading

--- app/(pokemon)/pokemon/page.tsx
+++ app/(pokemon)/pokemon/page.tsx
@@ -1,13 +1,9 @@
-import Loading from '@/components/loading';
-import { Suspense } from 'react';
 import PokemonDataTable from './pokemon-data-table';
 
 export default function Page() {
   return (
     <div className="md:min-w-sm">
-      <Suspense fallback={<Loading />}>
-        <PokemonDataTable />
-      </Suspense>
+      <PokemonDataTable />
     </div>
   );
 }
