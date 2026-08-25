65f764a10a685de61026610d1cbcd10e9f162627
lywoo-plus <sreng.lywoo@gmail.com>
2026-08-25 23:49:03 +0700

    feat: can view pokemon detail

--- app/(pokemon)/pokemon/[id]/page.tsx
+++ app/(pokemon)/pokemon/[id]/page.tsx
@@ -1,3 +1,59 @@
-export default function Page() {
-  return <div>detail</div>;
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { prisma } from '@/db/prisma';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
+
+export default async function Page({ params }: PageProps) {
+  const { id } = await params;
+  const pokemonId = Number(id);
+
+  if (!Number.isInteger(pokemonId)) {
+    notFound();
+  }
+
+  const pokemon = await prisma.pokemon.findUnique({
+    where: {
+      id: pokemonId,
+    },
+  });
+
+  if (!pokemon) {
+    notFound();
+  }
+
+  return (
+    <Card className="w-full md:w-sm">
+      <CardHeader>
+        {pokemon.imageUrl && (
+          <div className="bg-muted relative aspect-square w-full">
+            <Image
+              src={pokemon.imageUrl}
+              alt={pokemon.name}
+              fill
+              sizes="(max-width: 768px) 100vw, 384px"
+              className="object-cover"
+            />
+          </div>
+        )}
+        <CardTitle>{pokemon.name}</CardTitle>
+        <CardDescription>Pokemon #{pokemon.id}</CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <p className="text-muted-foreground text-sm leading-6">
+          {pokemon.description}
+        </p>
+      </CardContent>
+    </Card>
+  );
 }
---
+++ app/(pokemon)/pokemon/layout.tsx
@@ -0,0 +1,3 @@
+export default function Layout({ children }: LayoutProps<'/pokemon'>) {
+  return <>{children}</>;
+}
